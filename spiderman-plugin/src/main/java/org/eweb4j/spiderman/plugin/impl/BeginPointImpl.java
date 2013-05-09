95c24d61b2fe2a8e25423fc60f7468a7bc7f323a
laiweiwei <l.weiwei@163.com>
2013-05-09 12:20:34 +0800

    up logger of host validate

--- spiderman-plugin/src/main/java/org/eweb4j/spiderman/plugin/impl/BeginPointImpl.java
+++ spiderman-plugin/src/main/java/org/eweb4j/spiderman/plugin/impl/BeginPointImpl.java
@@ -12,12 +12,16 @@ import org.eweb4j.util.CommonUtil;
 
 public class BeginPointImpl implements BeginPoint{
 
+	private SpiderListener listener;
+	
 	public Task confirmTask(Task task) throws Exception{
 		//如果不是在给定的合法host列表里则不给于抓取
 		ValidHosts vhs = task.site.getValidHosts();
 		if (vhs == null || vhs.getValidHost() == null || vhs.getValidHost().isEmpty()){
-			if (!CommonUtil.isSameHost(task.site.getUrl(), task.url))
+			if (!CommonUtil.isSameHost(task.site.getUrl(), task.url)) {
+				listener.onInfo(Thread.currentThread(), task, "task.url->"+task.url+"'s host is not the same as site.host->" + task.site.getUrl());
 				return null;
+			}
 		}else{
 			boolean isOk = false;
 			String taskHost = new URL(task.url).getHost();
@@ -36,7 +40,7 @@ public class BeginPointImpl implements BeginPoint{
 	}
 
 	public void init(Site site, SpiderListener listener) {
-		
+		this.listener = listener;
 	}
 
 	public void destroy() {
